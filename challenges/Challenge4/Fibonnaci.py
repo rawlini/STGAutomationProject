b4ff99cd4fb79502bfda9e996b54a1c1e929a168
Ian Rawlings <irawlings@Ians-MacBook-Pro.local>
2019-12-12 16:48:23 -0700

    small changes

--- challenges/Challenge4/Fibonnaci.py
+++ challenges/Challenge4/Fibonnaci.py
@@ -1,10 +1,8 @@
-class Fibonnaci:
-    output=None
-    def fibonnaciSequence(self,Number):
-        a=0
-        b=1
-        i=1
-        while (i<= Number):
-            a,b= b+a,a
-            i=i+1
-        return b
+def Fibonnaci(Number):
+    a=0
+    b=1
+    i=1
+    while (i<= Number):
+        a,b= b+a,a
+        i=i+1
+    return b
--- challenges/Challenge4/challenge4.py
+++ challenges/Challenge4/challenge4.py
@@ -1,5 +1,5 @@
 import unittest
-from Fibonnaci import Fibonnaci as f
+from Fibonnaci import *
 from IntToString import IntToString as i
 from DriverInit import DriverInit as d
 
@@ -9,7 +9,7 @@ d.tearDown(d)
 class challenge4(unittest.TestCase):
 
     def test_challenge4(self):
-       i.intToString(i,f.fibonnaciSequence(f,50))
+       i.intToString(i,Fibonnaci(50))
 
 if __name__ == '__main__':
     unittest.main()
