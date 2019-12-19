be8e6b9162eeef7d3d6faca8a16dc67449686041
Ian Rawlings <irawlings@Ians-MacBook-Pro.local>
2019-12-19 09:14:05 -0700

    bonus sling images finished

--- challenges/bonus/challenge3BonusSlingImages.py
+++ challenges/bonus/challenge3BonusSlingImages.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class challenge1(unittest.TestCase):
 
@@ -10,8 +11,14 @@ class challenge1(unittest.TestCase):
         self.driver.close()
 
     def test_challenge1(self):
-        self.driver.get("https://www.google.com")
-        self.assertIn("Google", self.driver.title)
+        self.driver.get("https://www.sling.com")
+        listCount=len(self.driver.find_elements(By.XPATH, "//*[@id='channelList']/li/img"))
+        listCounter=1
+        while (listCounter <= listCount):
+            print(self.driver.find_element(By.XPATH, "//*[@id='channelList']/li["+str(listCounter)+"]/img").get_attribute('alt'))
+            if (listCounter == listCount):
+                break
+            listCounter += 1
 
 if __name__ == '__main__':
     unittest.main()
