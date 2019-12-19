e4180e3bfbf84c586379ea4fab3b74f4a75ef499
Ian Rawlings <irawlings@Ians-MacBook-Pro.local>
2019-12-19 09:55:25 -0700

    bonus challenge for sling search

--- challenges/bonus/challenge3BonusSlingSearch.py
+++ challenges/bonus/challenge3BonusSlingSearch.py
@@ -1,7 +1,11 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
-class challenge1(unittest.TestCase):
+class challenge2(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("chromedriver")
@@ -10,8 +14,18 @@ class challenge1(unittest.TestCase):
         self.driver.close()
 
     def test_challenge1(self):
-        self.driver.get("https://www.google.com")
-        self.assertIn("Google", self.driver.title)
+        self.driver.get("https://help.sling.com")
+        searchbar=self.driver.find_element(By.ID,"support-search-input")
+        searchbar.click()
+        searchbar.send_keys("TV")
+        searchbar.send_keys(Keys.RETURN)
+        WebDriverWait(self.driver, 10).until(
+                expected_conditions.visibility_of_element_located((By.XPATH,"//*[@class='search-results-list']"))
+            )
+        searchresult=self.driver.find_element(By.XPATH,"//ul[@class='search-results-list']/section/div/a")
+        if searchresult != None:
+            x=True    
+        self.assertTrue((x,"No Search Results found for TV"))
 
 if __name__ == '__main__':
     unittest.main()
